081e66c198feeea421c7c7e0ec013b0d559f6d5c
NhanHuynh <nhan123huynh2k3@gmail.com>
2023-03-03 23:58:24 +0700

    new version 230303

--- src/components/OurProduct/OurProduct.jsx
+++ src/components/OurProduct/OurProduct.jsx
@@ -9,18 +9,33 @@ export default function OurProduct() {
   const dispatch = useDispatch(); //dispatch(action); con voi creator: dispatch(action(payload));
   const myCart = useSelector(myCartSelector); //useSelector de lay ra du lieu ben trong kho luu tru
   const searchValStore = useSelector(searchProductSelector);
-  
+  const [isShowMore, setIsShowMore] = useState(false);
   const [productData, setProductData] = useState(undefined);
   const [tempData, setTempData] = useState(undefined);
 
+  const [widthWindow, setWidthWindow] = useState(window.innerWidth);
+
+  useEffect(()=> {
+    const resizeWidthHandle = () => {
+      setWidthWindow(window.innerWidth);
+    }
+
+    window.addEventListener("resize", resizeWidthHandle)
+
+    return () => window.removeEventListener("resize", resizeWidthHandle)
+  },[])
+
+  
+
   useEffect(() => {
     const prevData = tempData && [...tempData]; //dung bien temp Data de luu toan bo gia tri khoi tao, con product den hien thi
-    const productSearched = prevData && prevData.filter((product,index) => {
-      return product.name.includes(searchValStore);
-    })
+    const productSearched =
+      prevData &&
+      prevData.filter((product, index) => {
+        return product.name.includes(searchValStore);
+      });
     setProductData(productSearched);
-  },[searchValStore])
-  
+  }, [searchValStore]);
 
   useEffect(() => {
     fetch("https://fe21-db.vercel.app/funiro")
@@ -31,29 +46,46 @@ export default function OurProduct() {
       });
   }, []);
 
-  function handleAddToCart(product){
+  function handleAddToCart(product) {
     dispatch(addToCart(product));
   }
+  
 
   return (
     <div className={style.cardCover}>
       <h1 className={style.title}>Our Product</h1>
       <div className={style.cardContainer}>
         {productData ? (
-          productData.map((product, index) => (
-            <ItemCard
-              key={product._id}
-              id={product._id}
-              url={room1} //van chua doi lai theo img
-              title={product.name}
-              des={product.description}
-              priceAfter={product.discoutedPrice}
-              priceBefore={product.price}
-              discouter={product.discouter}
-              imgProduct={product.img}
-              handleAddToCart = {handleAddToCart}
-            />
-          ))
+          productData.filter((item,index) => {
+            if(isShowMore === false){
+              if(widthWindow <= 576){
+                return index < 4;
+              } 
+              else if(widthWindow > 576 && widthWindow <= 1274){
+                return index < 6;
+              } else {
+                return index < 8;
+              }
+              
+            } else {
+              return productData
+            }
+          }).map((product, index) => {
+            return (
+              <ItemCard
+                key={product._id}
+                id={product._id}
+                url={room1} //van chua doi lai theo img
+                title={product.name}
+                des={product.description}
+                priceAfter={product.discoutedPrice}
+                priceBefore={product.price}
+                discouter={product.discouter}
+                imgProduct={product.img}
+                handleAddToCart={handleAddToCart}
+              />
+            );
+          })
         ) : (
           <h1>Loading Data . . . . .</h1>
         )}
@@ -62,6 +94,7 @@ export default function OurProduct() {
         <button
           style={{ textTransform: "capitalize" }}
           className={style.btnShow}
+          onClick={() => setIsShowMore(!isShowMore)}
         >
           show more
         </button>
