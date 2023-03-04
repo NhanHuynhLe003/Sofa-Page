9de7cc56f35295c146fd40543ff53642ce9d9646
NhanHuynh <nhan123huynh2k3@gmail.com>
2023-03-04 16:11:05 +0700

    new version

--- src/pages/MainApp/App.jsx
+++ src/pages/MainApp/App.jsx
@@ -17,6 +17,8 @@ function App() {
       <Inspiration/>
       <TipAndTrickPage/>
       <SharePage/>
+      <h1>------------------------------------------------abcd</h1>
+      
     </div>
   )
 }
